8f40560a54d2f369c5efadef341ca2558500ac22
Evgeniy Kalashnikov <e.kalashnikov@a2b.direct>
2020-11-14 17:22:52 +0200

    not unique user name

--- src/main/resources/db/migration/V0__init.sql
+++ src/main/resources/db/migration/V0__init.sql
@@ -2,7 +2,7 @@ SET client_encoding = 'UTF8';
 
 create table users (
     id serial PRIMARY KEY,
-	name VARCHAR ( 100 ) UNIQUE NOT NULL,
+	name VARCHAR ( 100 ) NOT NULL,
 	password VARCHAR ( 100 ) NOT NULL
 );
 
