65cddbe98eda0835f139a6875de45844b1aa433b
Main <ibakay92@gmail.com>
2020-11-15 02:20:32 +0200

    auth reg /.

--- src/main/java/com/pharmacy/optican/demo/model/User.java
+++ src/main/java/com/pharmacy/optican/demo/model/User.java
@@ -13,11 +13,11 @@ import javax.persistence.*;
 @Builder
 public class User {
     @Id
-    @Column(name = "id")
+    @Column(name = "user_id")
     @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "user_seq")
     @SequenceGenerator(name = "user_seq", sequenceName = "user_id_seq", allocationSize = 1)
     private Integer id;
-    @Column(name = "name")
+    @Column(name = "username")
     private String name;
     @Column(name = "password")
     private String password;
